c74d6381710922d1ab83c60e0712d4488816ae1a
LJ <lji@tradeshift.com>
2015-01-08 16:42:24 +0800

    WIP

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
  * Module dependencies.
  */
 var express = require('express');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session')
+var bodyParser = require('body-parser');
+var cons = require('consolidate');
+var errorHandler = require('errorhandler');
+
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
@@ -15,24 +23,73 @@ var adpDocs = path.join(__dirname, '..', 'sdk', 'docs');
 app.set('port', process.env.PORT || 19819);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({
+	secret: 'don\'t let him know',
+	resave: false
+}));
+
+//app.use(express.Router());
 app.use(express.static(path.join(__dirname, 'public')));
 //add doc folder
 app.use('/android', express.static(path.join(__dirname, '..', 'sdk', 'docs')));
 app.use('/angularjs', express.static('/Users/liujing/myproject/angularJS/angular-1.2.13'));
+
+(function(){
+	function sendHtml(req, res){
+		res.sendfile(path.join('/Users/liujing/myproject/angularJS/angular-1.3.8', 'docs','index.html'));
+	}
+	app.route('/angularjs1.3/docs/api').get(sendHtml);
+	app.route('/angularjs1.3/docs/api/*').get(sendHtml);
+	app.route('/angularjs1.3/docs/guide/*').get(sendHtml);
+})();
+
+app.use('/angularjs1.3', express.static('/Users/liujing/myproject/angularJS/angular-1.3.8'));
 app.use('/yui3', express.static('/Users/liujing/myproject/yui_3.12.0'));
+app.use('/antlr3', express.static('/Users/liujing/myproject/antlr-3.3/api-doc'));
+app.use('/java7', express.static('/Users/liujing/doc/jdk7doc/docs'));
+app.use('/javatu', express.static('/Users/liujing/doc/javatutorial/tutorial'));
+app.use('/jedit', express.static('/Applications/jEdit.app/Contents/Resources/Java/doc/api'));
+//app.use('/test', express.static(path.join(__dirname, 'test')));
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', function(req, res){
     res.sendfile(path.join(__dirname, 'public','index.html'));
+});
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+    		console.log(err);
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 var server = http.createServer(app);
